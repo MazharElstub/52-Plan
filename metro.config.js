d1583f309c633ae4967cec0203bdda5eafbcdbe2
MazharElstub <thomas.elstub@gmail.com>
2025-05-30 12:01:23 +0100

    updates

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Reduce the number of watched files
+config.watchFolders = [];
+config.resolver.blockList = [
+  /node_modules\/.*\/node_modules/,
+  /\.git/,
+  /\.expo/,
+  /\.next/,
+];
+
 module.exports = config;
